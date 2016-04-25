aa493244cbcf7c7e85cc5bf83949a853f13d5302
colin-scott <cs@cs.berkeley.edu>
2016-04-25 15:20:42 -0700

    Write to CSV

--- how_many_relationships_changed.rb
+++ how_many_relationships_changed.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'set'
+require 'csv'
 
 class Top1000Filter
   def initialize
@@ -23,75 +24,108 @@ class Top1000Filter
   end
 end
 
-def compare_epochs(p2c1, p2c2, p2p1, p2p2, epoch1, epoch2, top_1000)
-  # Output seven categories:
-  # p2p -> p2c & p2p -> c2p
-  # p2p -> no longer present
-  # p2c -> p2p
-  # p2c -> no longer present
-  # p2c -> c2p & c2p -> p2c
-  # new p2p
-  # new p2c
-
-  total1 = p2p1 | p2c1
-  total2 = p2p2 | p2c2
-
-  # init from epoch 0, p2p and p2c from epoch 1
-  def disappeared(init,p2p,p2c)
-    init.select do |pair|
-      not (p2c.include? pair or p2c.include? [pair[1],pair[0]] or p2p.include? pair.sort)
-    end
+class Analyzer
+  def initialize
+    title_row = ["delta","p2p_dis","p2c_dis","p2p_changed","p2c_changed","flipped","new_p2p","new_p2c"]
+    @nop_csv = CSV.open("stacked_bar_chart/files/nop.csv", "wb")
+    @nop_csv << title_row
+    @nop_txt = File.open("nop_filter.txt","w")
+    @weak_csv = CSV.open("stacked_bar_chart/files/weak.csv", "wb")
+    @weak_csv << title_row
+    @weak_txt = File.open("weak_filter.txt","w")
+    @strict_csv = CSV.open("stacked_bar_chart/files/strict.csv", "wb")
+    @strict_csv << title_row
+    @strict_txt = File.open("strict_filter.txt","w")
+    @epoch = 0
   end
 
-  def p2pchanged(p2p1,p2c2)
-    p2p1.select do |pair|
-      p2c2.include? pair or p2c2.include? pair.sort
-    end
+  def close
+    @nop_csv.close
+    @nop_txt.close
+    @weak_csv.close
+    @weak_txt.close
+    @strict_csv.close
+    @strict_txt.close
   end
 
-  def p2cchanged(p2c1,p2p2)
-    p2c1.select do |pair|
-      p2p2.include? pair.sort
+  # Side effect: increments epoch counter
+  def compare_epochs(p2c1, p2c2, p2p1, p2p2, epoch1, epoch2, top_1000)
+    # Output seven categories:
+    # p2p -> p2c & p2p -> c2p
+    # p2p -> no longer present
+    # p2c -> p2p
+    # p2c -> no longer present
+    # p2c -> c2p & c2p -> p2c
+    # new p2p
+    # new p2c
+
+    total1 = p2p1 | p2c1
+    total2 = p2p2 | p2c2
+
+    # init from epoch 0, p2p and p2c from epoch 1
+    def disappeared(init,p2p,p2c)
+      init.select do |pair|
+        not (p2c.include? pair or p2c.include? [pair[1],pair[0]] or p2p.include? pair.sort)
+      end
     end
-  end
 
-  def flipped(p2c1,p2c2)
-    p2c1.select do |pair|
-      p2c2.include? [pair[1],pair[0]]
+    def p2pchanged(p2p1,p2c2)
+      p2p1.select do |pair|
+        p2c2.include? pair or p2c2.include? pair.sort
+      end
     end
-  end
 
-  def print_and_dump(dataset, output_file, dat, denom)
-    output_file.puts "%-30s %8d %01f" % [dataset, dat.size, (dat.size * 100.0 / denom)]
-
-    # File.open(filename, "w") do |f|
-    #   dat.each { |i| f.puts i.join(",") }
-    # end
-  end
-
-  ["nop_filter", "weak_filter", "strict_filter"].each do |dataset|
-    method = top_1000.method(dataset)
-    denom = total1.select { |i| method.call(i) }.size
-    # TODO(cs): performance improvement, could just open these files once...,
-    # close at the end.
-    File.open(dataset + ".txt", "a") do |f|
-      f.puts "====== #{epoch1} <-> #{epoch2} ======"
-      f.puts "denominator: #{denom}"
-      print_and_dump("p2p_disappeared", f,
-                     disappeared(p2p1,p2p2,p2c2).select { |i| method.call(i) }, denom)
-      print_and_dump("p2c_disappeared", f,
-                     disappeared(p2c1,p2p2,p2c2).select { |i| method.call(i) }, denom)
-      print_and_dump("p2p_changed", f,
-                     p2pchanged(p2p1,p2c2).select { |i| method.call(i) }, denom)
-      print_and_dump("p2c_changed", f,
-                     p2cchanged(p2c1,p2p2).select { |i| method.call(i) }, denom)
-      print_and_dump("flipped", f,
-                     flipped(p2c1,p2c2).select { |i| method.call(i) }, denom)
-      print_and_dump("new_p2p", f,
-                     disappeared(p2p2,p2p1,p2c1).select { |i| method.call(i) }, denom)
-      print_and_dump("new_p2c", f,
-                     disappeared(p2c2,p2p1,p2c1).select { |i| method.call(i) }, denom)
+    def p2cchanged(p2c1,p2p2)
+      p2c1.select do |pair|
+        p2p2.include? pair.sort
+      end
     end
+
+    def flipped(p2c1,p2c2)
+      p2c1.select do |pair|
+        p2c2.include? [pair[1],pair[0]]
+      end
+    end
+
+    def print_and_dump(dataset, output_file, dat, denom)
+      output_file.puts "%-30s %8d %01f" % [dataset, dat.size, percent(denom,dat)]
+
+      # File.open(filename, "w") do |f|
+      #   dat.each { |i| f.puts i.join(",") }
+      # end
+    end
+
+    def percent(denom, dat)
+      (dat.size * 100.0 / denom)
+    end
+
+    [["nop_filter",@nop_csv,@nop_txt], ["weak_filter",@weak_csv,@weak_txt], ["strict_filter",@strict_csv,@strict_txt]].each do |dataset,csv,txt|
+      method = top_1000.method(dataset)
+      denom = total1.select { |i| method.call(i) }.size
+      p2p_dis = disappeared(p2p1,p2p2,p2c2).select { |i| method.call(i) }
+      p2c_dis = disappeared(p2c1,p2p2,p2c2).select { |i| method.call(i) }
+      p2p_changed = p2pchanged(p2p1,p2c2).select { |i| method.call(i) }
+      p2c_changed = p2cchanged(p2c1,p2p2).select { |i| method.call(i) }
+      flipped = flipped(p2c1,p2c2).select { |i| method.call(i) }
+      new_p2p = disappeared(p2p2,p2p1,p2c1).select { |i| method.call(i) }
+      new_p2c = disappeared(p2c2,p2p1,p2c1).select { |i| method.call(i) }
+      txt.puts "====== #{epoch1} <-> #{epoch2} ======"
+      txt.puts "denominator: #{denom}"
+      print_and_dump("p2p_disappeared", txt, p2p_dis, denom)
+      print_and_dump("p2c_disappeared", txt, p2c_dis, denom)
+      print_and_dump("p2p_changed", txt, p2p_changed, denom)
+      print_and_dump("p2c_changed", txt, p2c_changed, denom)
+      print_and_dump("flipped", txt, flipped, denom)
+      print_and_dump("new_p2p", txt, new_p2p, denom)
+      print_and_dump("new_p2c", txt, new_p2c, denom)
+
+      # Epoch number,p2p_dis,p2c_dis,p2p_changed,p2c_changed,flipped,new_p2p,new_p2c
+      row = [@epoch, percent(denom,p2p_dis), percent(denom,p2c_dis), percent(denom,p2p_changed),
+             percent(denom,p2c_changed), percent(denom,flipped), percent(denom,new_p2p), percent(denom,new_p2c)]
+      csv << row
+    end
+
+    @epoch += 1
   end
 end
 
@@ -116,6 +150,7 @@ class DataFile
 end
 
 top_1000 = Top1000Filter.new
+analyzer = Analyzer.new
 
 # Assumes sorted filenames
 files = Dir.glob("*as-rel.txt")
@@ -135,7 +170,9 @@ while files.size >= 1
   p2p2 = new_data_object.p2p
   epoch1 = previous_data_object.filename
   epoch2 = new_data_object.filename
-  compare_epochs(p2c1, p2c2, p2p1, p2p2, epoch1, epoch2, top_1000)
+  analyzer.compare_epochs(p2c1, p2c2, p2p1, p2p2, epoch1, epoch2, top_1000)
 
   previous_data_object = new_data_object
 end
+
+analyzer.close
